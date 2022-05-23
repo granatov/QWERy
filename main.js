e15138b0cb8558ad3a788dd5e31efc7a2c9fbcc1
gena granatov <102759762+granatov@users.noreply.github.com>
2022-05-24 02:33:32 +0500

    add slider

--- main.js
+++ main.js
@@ -3,10 +3,20 @@ $(function () {
     $(".menu_brg,.menu_list,.header_social").toggleClass("active");
   });
 
-  // $('.photo_slider_items').slick({
-  //   dots: true,
-  //   infinite: true,
-  //   speed: 300,
-  //   slidesToShow: 1,
-  // });
+  $(".photo_slider_items").slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 880,
+        settings: {
+          arrows: false,
+          dots: true,
+        },
+      },
+    ],
+  });
 });
