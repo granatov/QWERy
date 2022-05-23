5d9126b6ac194744a2cbe77b6c649ab852536f41
gena granatov <102759762+granatov@users.noreply.github.com>
2022-05-23 22:56:20 +0500

    add respon

---
+++ main.js
@@ -0,0 +1,12 @@
+$(function () {
+  $(".menu_brg").click(function (event) {
+    $(".menu_brg,.menu_list,.header_social").toggleClass("active");
+  });
+
+  // $('.photo_slider_items').slick({
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 300,
+  //   slidesToShow: 1,
+  // });
+});
